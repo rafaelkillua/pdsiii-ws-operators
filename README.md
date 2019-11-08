c427b12eeacf515e3b39bcf5fbbf4b6c6e838bb7
Rafael Sampaio Tavares <rafael_killua@msn.com>
2019-11-07 21:43:15 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# WS-Operators
+
+## Scripts
+
+### npm start
+Cria as docs e abre o servidor, no endereço http://localhost:3333/api, e as docs em http://localhost:3333/docs
+
+### npm run dev
+Abre o servidor local de desenvolvimento, no endereço http://localhost:3333/api
+
+### npm run doc
+Gera as docs na pasta /doc
---
+++ routes/index.js
@@ -0,0 +1,94 @@
+const router = require('express').Router()
+
+/** 
+ * 
+ * 
+ * 
+ *  
+*/
+router.get('/', (req, res) => {
+  return res.send('index')
+})
+
+/**
+ * @api {POST} /pay/:operator Executa o pagamento via cartão de crédito
+ * @apiVersion 0.0.1
+ * @apiDescription Envia a solicitação para pagamento via cartão de crédito a operadora de cartão.
+ * @apiGroup Recursos Autenticados
+ * 
+ * @apiParam {String} operator Parâmetro da url que corresponde a operadora de cartão desejado. Deve ser uma das opções a seguir: op-01, op-02 ou op-03.
+ * @apiParam {String} numero_cartao Número do cartão de crédito.
+ * @apiParam {String} nome_cliente Nome do titular do cartão de crédito.
+ * @apiParam {String} bandeira Nome da bandeira segundo opções a seguir: mister (cod.: 1111), vista (cod.: 2222) ou daciolo (cod.: 3333).
+ * @apiParam {Number} cod_seguranca Código de três dígitos.
+ * @apiParam {Number} valor_em_centavos Valor em centavos da compra.
+ * @apiParam {Number} parcelas Quantidade de parcelas para o pagamento.
+ * @apiParam {String} cod_loja Código único da loja e-commerce. Será usado para que a operadora de cartão verifique se a loja é sua cliente. Deve ser uma das opções a seguir: loja-01, loja-02 ou loja-03.
+ * 
+ * @apiParamExample [JSON] Exemplo Corpo da Requisição
+ * {
+ *   "numero_cartao": "1111.2222.3333.4444",
+ *   "nome_cliente": "USUARIO DA SILVA",
+ *   "bandeira": "mister",
+ *   "cod_seguranca": 111,
+ *   "valor_em_centavos": 500,
+ *   "parcelas": 12,
+ *   "cod_loja": "loja-xx"
+ * }
+ * 
+ * @apiSuccess {String} resposta Resultado da transação.
+ * @apiSuccess {String} nome_cliente Nome do titular do cartão de crédito.
+ * @apiSuccess {Number} valor_em_centavos Valor em centavos da compra.
+ * @apiSuccess {Number} parcelas Quantidade de parcelas em que o pagamento foi feito.
+ * 
+ * @apiSuccessExample [JSON] Exemplo-Resposta-Sucesso
+ * {
+ *   "resposta": "sucesso",
+ *   "nome_cliente": "USUARIO DE SOUSA",
+ *   "valor_em_centavos": 500,
+ *   "parcelas": 12
+ * }
+ * 
+ * @apiError (401) {String} resposta Resultado da transação.
+ * @apiError (401) {String} detalhes Detalhes do erro
+ * @apiError (401) {String} operadora Operadora que foi buscado.
+ * @apiError (401) {String} cod-loja Loja de onde partiu a compra.
+ * @apiError (401) {String} bandeira Bandeira requisitada.
+ * @apiError (401) {String} parcelas_solicitadas Quantidade de parcelas solicitadas.
+ * @apiError (401) {String} limite_parcelas Limite de parcelas da bandeira.
+ * 
+ * @apiErrorExample [JSON] Resposta-Erro-Operadora-Inexistente
+ * {
+ *   "resposta": "falha",
+ *   "detalhes": "Operadora não existe",
+ *   "operadora": "op-xx"
+ * }
+ * @apiErrorExample [JSON] Resposta-Erro-Loja-Negada
+ * {
+ *   "resposta": "falha",
+ *   "detalhes": "Loja não autorizada",
+ *   "operadora": "op-xx",
+ *   "cod-loja": "loja-xx"
+ * }
+ * @apiErrorExample [JSON] Resposta-Erro-Bandeira-Negada
+ * {
+ *   "resposta": "falha",
+ *   "detalhes": "Bandeira não autorizada",
+ *   "operadora": "op-xx",
+ *   "bandeira": "mister"
+ * }
+ * @apiErrorExample [JSON] Resposta-Erro-Parcelas-Não-Aceitas
+ * {
+ *   "resposta": "falha",
+ *   "detalhes": "Limite de parcelas ultrapassado",
+ *   "bandeira": "mister"
+ *   "parcelas_solicitadas": 00,
+ *   "limite_parcelas": 00
+ * }
+ *  
+*/
+router.post('/pay/:operator', (req, res) => {
+  return res.send('pay operator')
+})
+
+module.exports = router
