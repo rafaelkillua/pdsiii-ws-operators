1d71fdd495a882b11e3e7bdcdb6460aad38b4792
Rafael Sampaio Tavares <rafael_killua@msn.com>
2019-11-22 00:06:48 -0300

    operators

--- src/controllers/ws-operators.js
+++ src/controllers/ws-operators.js
@@ -1,3 +1,5 @@
+const BrandsService = require('../services/ws-brands')
+
 module.exports = {
   status: (req, res) => {
     return res.status(200).json({ status: 'Serviço disponível WS1' })
@@ -34,7 +36,36 @@ module.exports = {
         cod_loja
       })
     }
+    
+    let limiteParcelas
+    try {
+      const response = await BrandsService.installmentsLimit(bandeira)
+      limiteParcelas = response.data.limite_parcelas
+    } catch (error) {
+      return res.status(400).json(error.message)
+    }
+    if (parcelas > limiteParcelas) {
+      return res.status(400).json({
+        resposta: 'falha',
+        detalhes: 'Bandeira não permite essa quantidade de parcelas',
+        parcelas,
+        bandeira
+      })
+    }
 
-    return res.status(200).json({ message: 'opa irmao, dando certo ai!' })
+    try {
+      const response = (await BrandsService.pay(bandeira, {
+        numero_cartao,
+        nome_cliente,
+        cod_seguranca,
+        valor_em_centavos,
+        parcelas,
+        cod_operadora: operadora
+      }))
+
+      return res.status(200).json(response.data)
+    } catch (error) {
+      return res.status(400).json(error.response.data)
+    }
   }
 }
